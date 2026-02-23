56151498c923e99060ce568789fc577b867c71f2
admirsaheta <admirxsaheta@gmail.com>
2026-02-23 11:51:38 +0100

    update:formula

--- Formula/sline-transpiler.rb
+++ Formula/sline-transpiler.rb
@@ -1,7 +1,7 @@
 class SlineTranspiler < Formula
   desc "Handlebars to Sline converter"
-  homepage "https://github.com/admirsaheta/homebrew-handlesline"
-  url "https://github.com/admirsaheta/homebrew-handlesline/blob/master/sline-transpiler-0.1.0-x86_64-apple-darwin.tar.gz"
+  homepage "https://github.com/admirsaheta/hombrew-handlesline"
+  url "https://github.com/admirsaheta/homebrew-handlesline/releases/download/v0.2.0/sline-transpiler-0.1.0-x86_64-apple-darwin.tar.gz"
   sha256 "855359fe74b7a93e7e029e87c1499c5bcc823b4636888279033c342bbde21558"
   license "MIT"
 
